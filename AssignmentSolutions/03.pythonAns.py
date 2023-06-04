0073bc33952521c55a32df95bed4c5b69eeefab4
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-06-04 08:20:49 +0530

    3. python solution

--- AssignmentSolutions/03.pythonAns.py
+++ AssignmentSolutions/03.pythonAns.py
@@ -1,20 +1,37 @@
+import pandas as pd
 import requests
 
-# Download the data from the provided link
+# Download the data from given link
 url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
 response = requests.get(url)
 data = response.json()
 
-# Extract relevant information from the JSON data
 pokemon_list = data["pokemon"]
-pokemon_data = []
-all_columns = set()
+structured_data = []
 
 for pokemon in pokemon_list:
-    pokemon_info = []
-    for key, value in pokemon.items():
-        if isinstance(value, (str, int, float)):
-            pokemon_info.append((key,value, type(value)))
-            all_columns.add(key)
-    pokemon_data.append(pokemon_info)
-print(pokemon_data[0])
+    pokemon_info = {
+        'id': pokemon['id'],
+        'num': pokemon['num'],
+        'name': pokemon['name'],
+        'img': pokemon['img'],
+        'type': ', '.join(pokemon['type']),
+        'height': pokemon['height'],
+        'weight': pokemon['weight'],
+        'candy': pokemon['candy'],
+        'candy_count': pokemon.get('candy_count', 0),
+        'egg': pokemon['egg'],
+        'spawn_chance': pokemon['spawn_chance'],
+        'avg_spawns': pokemon['avg_spawns'],
+        'spawn_time': pokemon['spawn_time']
+    }
+
+    structured_data.append(pokemon_info)
+
+df = pd.DataFrame(structured_data)
+
+df.to_excel('pokemon.xlsx', index=False)
+
+
+
+
