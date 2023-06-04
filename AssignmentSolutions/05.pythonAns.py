b00c5d29bb1c2c51b411a69db66a9626d2d0820e
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-06-04 10:19:08 +0530

    5. python solution

---
+++ AssignmentSolutions/05.pythonAns.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+# Download data from the API link
+url = "http://api.tvmaze.com/singlesearch/shows?q=westworld&embed=episodes"
+response = requests.get(url)
+data = response.json()
+
+# Extract relevant information from the data
+show_info = {
+    'id': data.get('id'),
+    'url': data.get('url'),
+    'name': data.get('name'),
+    'season': data.get('_embedded', {}).get('episodes', [{}])[0].get('season'),
+    'number': data.get('_embedded', {}).get('episodes',[{}])[0].get('number'),
+    'type': data.get('type'),
+    'airdate': data.get('_embedded', {}).get('episodes',[{}])[0].get('airdate'),
+    'airtime': data.get('_embedded', {}).get('episodes',[{}])[0].get('airtime'),
+    'runtime': data.get('runtime'),
+    'average rating': data.get('rating', {}).get('average', 'Unknown'),
+    'summary': data.get('summary', 'Unknown').strip().replace('<p>', '').replace('</p>', ''),
+    'medium image link': data.get('image', {}).get('medium', 'Unknown'),
+    'original image link': data.get('image', {}).get('original', 'Unknown')
+}
+
+# Print the extracted information
+for key, value in show_info.items():
+    print(f"{key}: {value}")
+
