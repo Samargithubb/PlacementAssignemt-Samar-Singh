84058dd9f856d2b6354f80e39587e64195163bcd
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-06-05 21:02:24 +0530

    mock test

---
+++ AssignmentSolutions/mock_test2.py
@@ -0,0 +1,23 @@
+def firstUniqChar(s):
+    char_count = {}
+
+    # Count the occurrences of each character
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+
+    # Find the first non-repeating character
+    for i, char in enumerate(s):
+        if char_count[char] == 1:
+            return i
+
+    return -1
+
+# Example test cases
+a1 = "leetcode"
+print(firstUniqChar(a1))
+
+a2 = "loveleetcode"
+print(firstUniqChar(a2))
+
+a3 = "aabb"
+print(firstUniqChar(a3))
